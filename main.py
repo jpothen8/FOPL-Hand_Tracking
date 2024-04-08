c721133e4c4e6700ea125125ad54f34a824f565b
John Joseph <69046000+jpothen8@users.noreply.github.com>
2024-04-08 13:40:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from video import threadVideo
 def main():
     xyz = threadVideo()
     xyz.start()
-
+    
     while True:
         # TODO GAME LOGIC
-        print(xyz.dist)
+        break
 
 if __name__ == '__main__':
     main()
