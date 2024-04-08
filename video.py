7d9386404d8f07fafaca349102e89d083a3a3070
John Joseph <69046000+jpothen8@users.noreply.github.com>
2024-04-08 13:39:46 -0400

    Update video.py

--- video.py
+++ video.py
@@ -1,5 +1,5 @@
-import cv2
 import math
+import cv2
 from threading import Thread
 import mediapipe as mp
 
@@ -45,29 +45,14 @@ class VideoShow:
                 self.stopped = True
 
     def stop(self):
-        self.stopped = True   
-
-def threadshow():
-
-    video_getter = VideoGet().start()
-    video_shower = VideoShow(video_getter.frame).start()  
-
-    while True:
-        if video_getter.stopped or video_shower.stopped:
-            video_shower.stop()
-            video_getter.stop()
-            break
-
-        frame = video_getter.frame
-
-        video_shower.frame = frame
-
+        self.stopped = True
+       
 class threadVideo:
 
     def __init__(self):
         self.video_getter = VideoGet().start()
         self.video_shower = VideoShow(self.video_getter.frame).start()
-        self.dist = 1.0
+        self.dist = float('inf')
 
     def start(self):
         Thread(target=self.show, args=()).start()
@@ -94,18 +79,35 @@ class threadVideo:
                 # Draw the hand annotations on the image.
                 image.flags.writeable = True
                 image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-                dist= 1.0
+                dist= float('inf')
                 if results.multi_hand_landmarks:
                     for hand_landmarks in results.multi_hand_landmarks:
                         thumb = hand_landmarks.landmark[4]
                         middle = hand_landmarks.landmark[10]
-                        dist = math.sqrt((thumb.x - middle.x)**2 + (thumb.y - middle.y)**2)
+
+                        bottom = hand_landmarks.landmark[0]
+                        palm = hand_landmarks.landmark[9]
+
+
+                        dist = math.sqrt((thumb.x - middle.x)**2 + (thumb.y - middle.y)**2) / math.sqrt((bottom.x - palm.x)**2 + (bottom.y - palm.y)**2)
                         mp_drawing.draw_landmarks(
                             image,
                             hand_landmarks,
                             mp_hands.HAND_CONNECTIONS,
                             mp_drawing_styles.get_default_hand_landmarks_style(),
                             mp_drawing_styles.get_default_hand_connections_style())
+                        
+                cv2.putText(
+                    image, 
+                    "Distance: " + str(round(dist, 5)), 
+                    (50, 50), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 
+                    1, 
+                    (255, 0, 255), 
+                    2, 
+                    cv2.LINE_4
+                )
+
                 self.video_shower.frame = image
                 self.dist = dist
 if __name__ == '__main__':   
